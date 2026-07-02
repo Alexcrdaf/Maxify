00a302c8c68e8ec9b023bc2bfc6aac8a9edada0a
Alexcrdaf <158591299+Alexcrdaf@users.noreply.github.com>
2026-07-02 20:45:49 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -22,3 +22,19 @@ self.addEventListener('push', e => {
         })
     );
 });
+
+self.addEventListener('notificationclick', e => {
+    e.notification.close();
+    e.waitUntil(
+        self.clients.matchAll({ type: 'window', includeUncontrolled: true }).then(clientList => {
+            for (const client of clientList) {
+                if (client.url.includes(self.location.origin) && 'focus' in client) {
+                    return client.focus();
+                }
+            }
+            if (self.clients.openWindow) {
+                return self.clients.openWindow('/');
+            }
+        })
+    );
+});
